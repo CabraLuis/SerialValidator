714aba47f94c41209babaf3c156c7ad614d15b10
Luis Cabra <zetinaluis03@gmail.com>
2024-09-03 14:23:47 -0500

    upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace SerialValidator
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumbers = new System.Windows.Forms.RichTextBox();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.pbQrCode = new System.Windows.Forms.PictureBox();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.btnGenerarPDF = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(12, 12);
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.Size = new System.Drawing.Size(150, 426);
+            this.txtNumbers.TabIndex = 0;
+            this.txtNumbers.Text = "";
+            this.txtNumbers.WordWrap = false;
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidate.Location = new System.Drawing.Point(168, 12);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(199, 30);
+            this.btnValidate.TabIndex = 1;
+            this.btnValidate.Text = "Validar Seriales";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // pbQrCode
+            // 
+            this.pbQrCode.Location = new System.Drawing.Point(168, 238);
+            this.pbQrCode.Name = "pbQrCode";
+            this.pbQrCode.Size = new System.Drawing.Size(200, 200);
+            this.pbQrCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbQrCode.TabIndex = 3;
+            this.pbQrCode.TabStop = false;
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = global::SerialValidator.Properties.Resources.Thermx_Logo_No_Background;
+            this.pbLogo.Location = new System.Drawing.Point(167, 84);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(200, 148);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogo.TabIndex = 4;
+            this.pbLogo.TabStop = false;
+            // 
+            // btnGenerarPDF
+            // 
+            this.btnGenerarPDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarPDF.Location = new System.Drawing.Point(168, 48);
+            this.btnGenerarPDF.Name = "btnGenerarPDF";
+            this.btnGenerarPDF.Size = new System.Drawing.Size(199, 30);
+            this.btnGenerarPDF.TabIndex = 5;
+            this.btnGenerarPDF.Text = "Generar PDF";
+            this.btnGenerarPDF.UseVisualStyleBackColor = true;
+            this.btnGenerarPDF.Click += new System.EventHandler(this.btnGenerarPDF_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.Controls.Add(this.btnGenerarPDF);
+            this.Controls.Add(this.pbLogo);
+            this.Controls.Add(this.pbQrCode);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.txtNumbers);
+            this.Name = "mainForm";
+            this.Text = "Serial Validator";
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtNumbers;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.PictureBox pbQrCode;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Button btnGenerarPDF;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.UniversalAccessibility.Drawing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
+
+namespace SerialValidator
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string conn = "Data Source = C:\\Users\\itmex\\OneDrive - NIBE\\Escritorio\\database.db;Version = 3;";
+                SQLiteConnection connection = new SQLiteConnection(conn);
+                connection.Open();
+
+                SQLiteCommand insertCommand = new SQLiteCommand(connection);
+                SQLiteCommand selectCommand = new SQLiteCommand(connection);
+                selectCommand.CommandText = "SELECT number FROM Numbers WHERE number = @serial";
+                insertCommand.CommandText = "INSERT INTO Numbers (number) VALUES (@serial)";
+
+                List<string> duplicates = new List<string>();
+                string currentSerial;
+
+                for (int i = 0; i < txtNumbers.Lines.Length; i++)
+                {
+                    currentSerial = txtNumbers.Lines[i];
+                    selectCommand.Parameters.AddWithValue("serial", currentSerial);
+                    insertCommand.Parameters.AddWithValue("serial", currentSerial);
+                    SQLiteDataReader reader = selectCommand.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string serial = reader.GetString(0);
+                            duplicates.Add(serial);
+                            Console.WriteLine(serial);
+                        }
+                        reader.Close();
+                    }
+                    else
+                    {
+                        reader.Close();
+                        insertCommand.ExecuteNonQuery();
+                    }
+
+                    if (currentSerial == null)
+                    {
+                        throw new Exception();
+                    }
+
+                    Bitmap generatedBarcode = GenerateBarcode(currentSerial);
+                    XImage barcodeTemp = XImage.FromGdiPlusImage(generatedBarcode);
+
+
+                }
+                connection.Close();
+                
+                pbQrCode.Image = GenerateQR();
+                
+                if (duplicates.Count() != 0)
+                {
+                    string duplicatesString = string.Join("\n", duplicates);
+                    MessageBox.Show("Seriales Duplicados:\n" + duplicatesString, "Duplicados Encontrados", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnGenerarPDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string currentSerial;
+
+                PdfDocument document = new PdfDocument();
+                PdfPage page = document.AddPage();
+                page.Orientation = PdfSharp.PageOrientation.Landscape;
+                XGraphics gfx = XGraphics.FromPdfPage(page);
+                XFont titleFont = new XFont("Verdana", 18, XFontStyleEx.Bold);
+                XFont serialFont = new XFont("Verdana", 5, XFontStyleEx.Regular);
+                gfx.DrawString("Verificador de Seriales", titleFont, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.TopCenter);
+
+                double xImage = 1;
+                double yImage = 0.7;
+
+                for (int i = 0; i < txtNumbers.Lines.Length; i++)
+                {
+                    currentSerial = txtNumbers.Lines[i];
+
+                    if (currentSerial == null)
+                    {
+                        throw new Exception();
+                    }
+
+                    Bitmap generatedBarcode = GenerateBarcode(currentSerial);
+                    XImage barcodeTemp = XImage.FromGdiPlusImage(generatedBarcode);
+
+                    //gfx.DrawString(currentSerial, serialFont, XBrushes.Black, );
+                    gfx.DrawImage(barcodeTemp, xImage * 93.5, yImage * 75);
+
+                    yImage += 1.5;
+
+                    Console.WriteLine(barcodeTemp.Size);
+
+                    if (yImage >= 8)
+                    {
+                        yImage = 0.7;
+                        xImage += 1.5;
+                    }
+
+                }
+
+                pbQrCode.Image = GenerateQR();
+
+                FolderBrowserDialog dialog = new FolderBrowserDialog();
+                dialog.ShowDialog();
+
+                string filename = dialog.SelectedPath + "\\Seriales.pdf";
+                Console.WriteLine(filename);
+                document.Save(filename);
+                ProcessStartInfo startInfo = new ProcessStartInfo(filename);
+                Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Bitmap GenerateBarcode(string serial)
+        {
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.CODE_128;
+            barcodeWriter.Options.PureBarcode = true;
+            barcodeWriter.Options.Margin = 1;
+            Bitmap barcodeBitmap = barcodeWriter.Write(serial);
+            return barcodeBitmap;
+        }
+
+        private Bitmap GenerateQR()
+        {
+            var barcodeWriter = new BarcodeWriter();
+            
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions()
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 300,
+                Height = 300
+            };
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+            barcodeWriter.Options = options;
+
+            string concatNumbers = string.Join(Environment.NewLine, txtNumbers.Lines);
+            var qrCodeBitmap = barcodeWriter.Write(concatNumbers);
+
+            return qrCodeBitmap;
+        }
+
+       
+    }
+}
